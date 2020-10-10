6be615b021f5a4380a1f4c8dec5b2999607935c4
Mike <michael_eric_moore@hotmail.com>
2020-10-10 13:18:04 -0500

    add cloudfront invalidation to publish script

--- publish.bash
+++ publish.bash
@@ -2,7 +2,13 @@ aws s3 sync ./ s3://catalystcode.io/ \
   --exclude '*' \
   --include 'index.html' \
   --include 'favicon*' \
+  --include 'logo*' \
   --cache-control max-age=604800 \
   --metadata-directive REPLACE \
   --delete \
+  $@
+
+AWS_PAGER="" aws cloudfront create-invalidation \
+  --distribution-id E2FVN06WF0XDRV \
+  --paths '/' '/*' \
   $@
